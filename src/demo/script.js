7eeb7df381be32fb314c98f49f4981d277ffe0a2
Klem26 <Kankina26@gmail.com>
2021-11-09 23:38:10 +0200

    +

--- src/demo/script.js
+++ src/demo/script.js
@@ -3,6 +3,9 @@ import MagneticHover from "../js/magneticHover";
 //init
 const elem = document.getElementById("box");
 const distanceBox = document.getElementById("distance");
+const coordinateX = document.getElementById("coordinateX");
+const coordinateY = document.getElementById("coordinateY");
+
 if (elem) {
   new MagneticHover({
     element: elem,
@@ -12,3 +15,16 @@ if (elem) {
     },
   });
 }
+
+function onMousemove(event) {
+  let bounds = event.target.getBoundingClientRect();
+  let width = 60 / 2;
+  let height = 30 / 2;
+  let x = event.clientX - (bounds.left + width);
+  let y = event.clientY - (bounds.top + height);
+  coordinateX.innerText = x;
+  coordinateY.innerText = y;
+  return;
+}
+
+window.addEventListener("mousemove", onMousemove, false);
